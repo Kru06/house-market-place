67d073e4a80e780173e65d6db10c35fda3a7cf8e
Kru06 <89855611+Kru06@users.noreply.github.com>
2023-08-07 11:16:45 +0530

    Just hotfix

--- src/components/Layout/Footer.js
+++ src/components/Layout/Footer.js
@@ -3,7 +3,7 @@ import React from "react";
 const Footer = () => {
   return (
     <div id="cright" className="d-flex align-items-center justify-content-center text-light p-4">
-      <h6>Made By Harshil & Naitik </h6>
+      <h6>Made By Krupa Rana</h6>
     </div>
   );
 };
